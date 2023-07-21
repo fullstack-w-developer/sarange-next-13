4256d5225c35fbf0423730baa32f399efc628de4
mahdi-shariflo <78957548+mahdi-shariflo@users.noreply.github.com>
2023-07-21 15:10:10 +0330

    auth

--- hooks/query/drivers/useGetDriverByCityNumberQuery.ts
+++ hooks/query/drivers/useGetDriverByCityNumberQuery.ts
@@ -1,28 +1,27 @@
 import { useQuery } from "react-query";
 import { useRouter } from "next/navigation";
 import { getDriverByCityName } from "@/services/driver";
-import { DRIVER } from "@/helpers/constants/query-keys";
-import useDriverStore from "@/stores/driver-store";
-import { errorToast } from "@/helpers/utils/error";
+// import { DRIVER } from "@/helpers/constants/query-keys";
+// import useDriverStore from "@/stores/driver-store";
 
 const useGetDriverByCityNumberQuery = () => {
-    const { code } = useDriverStore();
+    // const { code } = useDriverStore();
     const router = useRouter();
-    const { setDriver } = useDriverStore();
-    return useQuery([DRIVER.GETDRIVE_BY_CITYNUMBER, code], async () => await getDriverByCityName(code), {
-        enabled: Boolean(code),
-        cacheTime: 0,
-        staleTime: 0,
-        retry: 0,
-        onSuccess: async (data) => {
-            if (!data?.CarRoute?.Name) return;
-            await setDriver(data);
-            router.push(`/user/driver/info`);
-        },
-        onError: (error) => {
-            // return errorToast("راننده ای یا این کد شهری یافت نشد");
-        },
-    });
+    // const { setDriver } = useDriverStore();
+    // return useQuery([DRIVER.GETDRIVE_BY_CITYNUMBER, code], async () => await getDriverByCityName(code), {
+    //     enabled: Boolean(code),
+    //     cacheTime: 0,
+    //     staleTime: 0,
+    //     retry: 0,
+    //     onSuccess: async (data) => {
+    //         if (!data?.CarRoute?.Name) return;
+    //         await setDriver(data);
+    //         router.push(`/user/driver/info`);
+    //     },
+    //     onError: (error) => {
+    //         // return errorToast("راننده ای یا این کد شهری یافت نشد");
+    //     },
+    // });
 };
 
 export default useGetDriverByCityNumberQuery;
