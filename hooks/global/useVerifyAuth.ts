0facd5ee86fbf4f5aec7e5b23293832dbccc2942
mahdi-shariflo <78957548+mahdi-shariflo@users.noreply.github.com>
2023-07-22 19:42:38 +0330

    middelware

--- hooks/global/useVerifyAuth.ts
+++ hooks/global/useVerifyAuth.ts
@@ -1,16 +1,18 @@
+import { useRouter } from "next/navigation";
 import axios from "../../services/utils/axios";
 import { useEffect } from "react";
 import { useCookies } from "react-cookie";
 
 const useVerifyAuth = () => {
+    const router = useRouter()
     const [cookies] = useCookies(["token"]);
-    function disableBackButton() {
-        window.history.replaceState(null, document.title, window.location.href);
-        window.addEventListener('popstate', function (event) {
-            event.preventDefault();
-            window.history.forward();
-        });
-    }
+    // function disableBackButton() {
+    //     window.history.replaceState(null, document.title, window.location.href);
+    //     window.addEventListener('popstate', function (event) {
+    //         event.preventDefault();
+    //         window.history.forward();
+    //     });
+    // }
     const checkToken = async () => {
         if (cookies.token) {
             axios.defaults.headers.common["x-access-token"] = `${cookies.token}`;
@@ -18,9 +20,24 @@ const useVerifyAuth = () => {
     };
     useEffect(() => {
         checkToken();
-        disableBackButton()
+        // disableBackButton()
 
     }, [cookies.token]);
+    useEffect(() => {
+        const disableBrowserBackButton = (event: any) => {
+            event.preventDefault();
+            router.push('/');
+        };
+        // @ts-ignore
+        window.history.pushState(null, null, window.location.pathname);
+        window.addEventListener('popstate', disableBrowserBackButton);
+
+        return () => {
+            window.removeEventListener('popstate', disableBrowserBackButton);
+        };
+    }, [router]);
 };
+
+
 
 export default useVerifyAuth;
