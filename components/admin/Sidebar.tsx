e355dd4ffb2e991e6bec1cf5f8dc8f81f1be9086
developer <mahdilove279@gmail.com>
2023-08-16 11:18:55 +0330

    fix menu

--- components/admin/Sidebar.tsx
+++ components/admin/Sidebar.tsx
@@ -17,6 +17,7 @@ export default function Sidebar({ dashboardAdmin }: Props) {
     const [open, setOpen] = useState(false)
     const pathname = usePathname()
     const access = dashboardAdmin[0]?.Attributes?.find((menu: any) => menu.Value === "access")
+    console.log(dashboardAdmin,"wwwwwwwwwwwwwwwwwwwwwwwwwwww")
     return (
         <div className="bg-white hidden border border-gray-200  lg:py-4 px-2 h-fit rounded-lg lg:flex lg:flex-col justify-between items-center">
             <div className="w-full">
@@ -39,23 +40,7 @@ export default function Sidebar({ dashboardAdmin }: Props) {
                     </div>
                 </div>
                 <ul className="hidden w-full lg:block mt-10 space-y-3">
-                    {dashboardAdmin[0].Attributes.map((item: any, index: any) => {
-                        if (item.Value === "access") return
-                        return (
-                            <li key={index}>
-                                <Link
-                                    href={`/admin/${item.Value}`}
-                                    className={`flex gap-4 items-center px-6 mx-2 py-2 text-[14px] ${pathname === `/admin/${item.Value}`
-                                        ? "font-artin-bold text-[#1F0990] after:rounded-xl relative after:absolute after:right-0 after:w-[3px] after:h-6 after:bg-[#02D0FF]"
-                                        : "font-artin-regular text-[#222222]"
-                                        }`}
-                                >
-                                    {/* {true ? <item.iconFill /> : <item.icon />} */}
-                                    {item.Name}
-                                </Link>
-                            </li>
-                        );
-                    })}
+                 
                     {access &&
                         <div className="flex flex-col items-center justify-between">
                             <button onClick={()=> setOpen(!open)} className={`flex justify-between w-full gap-4 items-center px-6 mx-2 py-2 text-[14px] ${open ? "font-artin-bold text-[#1F0990] after:rounded-xl relative after:absolute after:right-0 after:w-[3px] after:h-6 after:bg-[#02D0FF]"
